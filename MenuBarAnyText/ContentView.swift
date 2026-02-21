dea23a5229d13c32e77eb5b924eaddc86e9ff178
Dirk Clemens <git@adcore.de>
2026-02-21 12:52:36 +0100

    Initial Commit

---
+++ MenuBarAnyText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MenuBarAnyText
+//
+//  Created by Dirk Clemens on 21.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MenuBarAnyText/MenuBarAnyTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarAnyTextApp.swift
+//  MenuBarAnyText
+//
+//  Created by Dirk Clemens on 21.02.26.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarAnyTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
